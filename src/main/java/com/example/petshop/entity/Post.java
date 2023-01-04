63db49994f6a5e1cb4d2b01d6b9ce4ad87c3534e
Duong Phuong <duongtuanphuong2@gmail.com>
2023-01-04 23:33:34 +0700

    pet-shop

---
+++ src/main/java/com/example/petshop/entity/Post.java
@@ -0,0 +1,35 @@
+package com.example.petshop.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private String content;
+
+    @Lob
+    private byte[] thumbnail;
+
+    
+}
